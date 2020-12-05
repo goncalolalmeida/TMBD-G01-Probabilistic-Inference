db3f8c8afc7279a937640ea3e4975a7c88611d10
goncalolalmeida <goncalo.almeida@ua.pt>
2020-12-05 18:50:58 +0000

    fixed some vars

--- problems.py
+++ problems.py
@@ -26,30 +26,28 @@ print(M.succ(), end='\n\n')
 C = pykov.Chain( dic )
 state = pykov.Vector(S=1)
 # Distribuição de prob após N dias, começando no estado state
-n_inicio=274 # dias, entre o dia do primeiro infetado em Portugal e o dia da submissão do projeto
+n_inicio=282 # dias, entre o dia do primeiro infetado em Portugal e o dia da submissão do projeto
 dia274=C.pow(state, n_inicio)
 n_fim=365-(31+28+1)
 fim2020=C.pow(state, n_fim)
 
 # % aumento de recuperados esperado entre 09/12/2020 e 01/01/2021
-rate_r=(fim2020-dia274)['R']
+rate_r=(fim2020-dia274)['R']*100
 # média de recuperados diários esperada entre 09/12/2020 e 01/01/2021
 pop = 10.28*10**6
 dias = n_fim - n_inicio
-avg_r_daily=(rate_r * pop / dias)
+avg_r_daily=(rate_r / 100 * pop / dias)
+print('rate_r {}\navg_r_daily {}\n'.format(round(rate_r,2), round(avg_r_daily)))
 
 
-# Random walk de N transições; opcional: começar no estado A e acabar no estado B
-n=1
-C.walk(n, 'I', 'R') #descartar?
 
+n=10
 
 # Prob ( recuperar no primeiro dia após ser infetado )
 walk = ['I', 'R']
 p1=math.exp(C.walk_probability(walk))
 
 # Prob( um infetado recuperar no Nº dia )
-n=10
 walk = ['I']*n + ['R']
 p2=math.exp(C.walk_probability(walk))
 
@@ -59,28 +57,28 @@ walk[1:1] = ['I']*m
 p3=sum( [math.exp(C.walk_probability(walk[i:])) for i in range(m+1)] )
 
 # Prob ( um infetado passar menos de N dias (seguintes) hospitalizado)
-n=5
 walk = ['I'] + ['H']*n
 p4=sum( [ math.exp(C.walk_probability(walk[:-i])) for i in range(1, n) ])
 
 # Prob ( estando infetado ou hospitalizado, passar apenas os 2 dias seguintes em UCI )
-walks = [['H','U','U','H'], ['H','U','U','I'], ['H','U','U','O'], ['I','U','U','O']]
+walks = [['H','U','U','H'], ['H','U','U','O'], ['I','U','U','H'], ['I','U','U','O']]
 p5=sum([math.exp(C.walk_probability(w)) for w in walks])
 
-# Prob ( entrar em UCI e morrer em menos de N+1 dias, sabendo estava infetado/hospitalizado )
-n=9
-walks = [ ['H'] + ['U']*n + ['O'], ['I'] + ['U']*n + ['O'] ]
-p6=sum([math.exp(C.walk_probability(w[:1]+w[1+h:len(w)])) for w in walks for h in range(n-1)])
+# Prob ( entrar em UCI e morrer em menos de N dias, sabendo estava infetado/hospitalizado )
+walks = [ ['H'] + ['U']*(n-1) + ['O'], ['I'] + ['U']*(n-1) + ['O'] ]
+p6=sum([math.exp(C.walk_probability(w[:1]+w[1+h:len(w)])) for w in walks for h in range(n-2)])
+
+print('p1 {}\np2 {}\np3 {}\np4 {}\np5 {}\np6 {}\n'.format(round(p1,2),round(p2,2), round(p3,2), round(p4,2), round(p5,2), round(p6,2)))
 
 
 
 # Mean First Passage Times
 
-steps=10**5
-times=10**4
+days=10**4
+times=10**5
 
 # S -> I (~99 ~101)
-print(np.mean([len(C.walk(steps,'S','I'))-1 for i in range(times)]))
+print('MFPG (S->I) {}'.format(round(np.mean([len(C.walk(days,'S','I'))-1 for i in range(times)]))))
 # I -> R
 #print(np.mean([len(C.walk(steps,'I','R'))-1 for i in range(times)]))
 # I -> H
@@ -99,4 +97,5 @@ print(np.mean([len(C.walk(steps,'S','I'))-1 for i in range(times)]))
 days=10**4
 v_states = [pykov.Vector(S=1), pykov.Vector(I=1), pykov.Vector(H=1), pykov.Vector(U=1), pykov.Vector(O=1), pykov.Vector(R=1)]
 steady = dict(zip(str_states, [C.pow(init, days) for init in v_states]))
+
 
